0019f9a4e7313cbbb921a56c7f3d3e2881a8cef5
Katya Katsy <kkatsy@ucsd.edu>
2023-07-05 12:58:01 -0700

    Update deeplab.py

--- deeplab.py
+++ deeplab.py
@@ -9,9 +9,12 @@ from collections import OrderedDict
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
-class DeepLabV3P(nn.Module):
-    def __init__(self, n_class):
-        super(DeepLabV3P, self).__init__()
+class DeepLabV3Plus(nn.Module):
+    def __init__(self, n_classes, n_channels, bilinear):
+        super(DeepLabV3Plus, self).__init__()
+        self.n_classes = n_classes
+        self.n_channels = n_channels
+        self.bilinear = bilinear
 
         # ENCODER
         # load in pretrained model
@@ -29,15 +32,15 @@ class DeepLabV3P(nn.Module):
         self.begin_resnet_layers = nn.ModuleList([resnet.conv1, resnet.bn1, resnet.relu, resnet.maxpool])
 
         # resnet high-level
-        self.resnet_4_layers = nn.ModuleList([resnet.layer1, resnet.layer2, resnet.layer3]) #, resnet.layer4])
+        self.resnet_4_layers = nn.ModuleList([resnet.layer1, resnet.layer2, resnet.layer3 , resnet.layer4])
         features_4 = self.begin_resnet_layers + self.resnet_4_layers
         self.resnet_4 = torch.nn.Sequential(*features_4)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        print(self.resnet_4_layers)
+        # print(self.resnet_4_layers)
 
         # ASPP
         # 1 x 1 conv
-        self.aspp_conv1 = Conv2d(1024, 256, kernel_size=1, padding='same', bias=False)
+        self.aspp_conv1 = Conv2d(2048, 256, kernel_size=1, padding='same', bias=False)
         self.aspp_bn1 = BatchNorm2d(256)
         self.relu = ReLU(inplace=True)
 
@@ -48,46 +51,53 @@ class DeepLabV3P(nn.Module):
         # 3 x 3 conv, rate 18
         self.aspp_conv3_r18 = Conv2d(256, 256, 3, padding=18, dilation=18, bias=False)
         # image pooling
-        self.aspp_pool = AdaptiveAvgPool2d((16, 16))
+        self.aspp_pool = AdaptiveAvgPool2d((8, 8))
 
         # ENCODER OUT
         # concat ASPP
-        self.upsample_resnet = Upsample(scale_factor=32) #, mode='bilinear')
+        if self.bilinear:
+          self.upsample_resnet = Upsample(scale_factor=32, mode='bilinear')
+        else:
+          self.upsample_resnet = Upsample(scale_factor=32)
+
         # 1x1 conv ASPP
         self.conv1_encoder = Conv2d(5 * 256, 256, 1, bias=False)
         self.b1_encoder = BatchNorm2d(256)
 
         # resnet low-level
-        self.resnet_2_layers = ModuleList([resnet.layer1]) #, resnet.layer2])
+        self.resnet_2_layers = ModuleList([resnet.layer1, resnet.layer2])
         features_2 = self.begin_resnet_layers + self.resnet_2_layers
         self.resnet_2 = torch.nn.Sequential(*features_2)
 
         # DECODER
         # resnet output 1x1 conv
-        self.low_level_conv = Conv2d(256, 256, 1, bias=False)
+        self.low_level_conv = Conv2d(512, 256, 1, bias=False)
         self.low_level_bn = BatchNorm2d(256)
+        
         # upsample conv'd ASPP output
-        self.upsample_encoder = Upsample(scale_factor=4) #, mode='bilinear')
+        if self.bilinear:
+          self.upsample_encoder = Upsample(scale_factor=4, mode='bilinear')
+        else:
+          self.upsample_encoder = Upsample(scale_factor=4) 
+
         # concat resnet + ASPP
         # 3 x 3 conv
-        self.conv3_decoder = Conv2d(512, 256, 3, padding=1, bias=False)
-        self.bn_decoder = BatchNorm2d(256)
+        self.conv3_decoder = Conv2d(512, self.n_classes, 3, padding=1, bias=False)
+        self.bn_decoder = BatchNorm2d(self.n_classes)
+
         # upsample by 4
-        self.upsample_decoder = Upsample(scale_factor=4) #, mode='bilinear')
+        if self.bilinear:
+          self.upsample_decoder = Upsample(scale_factor=8, mode='bilinear')
+        else:
+          self.upsample_decoder = Upsample(scale_factor=8) 
 
     def forward(self, input):
-        print("Initial: ", input.shape)
-        # ENCODER
-        # ResNet High-Level
+
         x1 = self.in_conv(input)
         x1 = self.in_bn(x1)
-        # output to use for unet
         x1 = self.in_relu(x1)
-        # x_s[0]: [batch_size x 64 x 128 x 128]
 
         pre_resnet = self.in_maxpool(x1)
-        # => x: [batch_size x 64 x 64 x 64]
-        print("Post Pre-ResNet: ", pre_resnet.shape)
 
         in_vals, out_resnet4 = 0, 0
         for i, layer in enumerate(self.resnet_4_layers):
@@ -96,12 +106,10 @@ class DeepLabV3P(nn.Module):
             else:
                 in_vals = out_resnet4
             out_resnet4 = layer(in_vals)
-        print("Post ResNet: ", out_resnet4.shape)
 
         x2 = self.aspp_conv1(out_resnet4)
         x2 = self.aspp_bn1(x2)
         aspp_conv1_output = self.relu(x2)
-        print("Mid ASPP: ", aspp_conv1_output.shape)
 
         x3 = self.aspp_conv3_r6(aspp_conv1_output)
         x3 = self.aspp_bn1(x3)
@@ -116,19 +124,15 @@ class DeepLabV3P(nn.Module):
         aspp_conv3_r18_output = self.relu(x5)
 
         aspp_pool_output = self.aspp_pool(aspp_conv3_r18_output)
-        print("Post ASPP: ", aspp_pool_output.shape)
 
         aspp_pyramid = [aspp_conv1_output, aspp_conv3_r6_output, aspp_conv3_r12_output, aspp_conv3_r18_output, aspp_pool_output]
         aspp_concat = torch.cat(aspp_pyramid, dim=1)
-        print("Post Concat: ", aspp_concat.shape)
 
         x6 = self.conv1_encoder(aspp_concat)
         x6 = self.b1_encoder(x6)
         x6 = self.relu(x6)
-        print("Post Encoder Conv: ", x6.shape)
 
         upsampled_encoder = self.upsample_encoder(x6)
-        print("Post Encoder Upsample: ", upsampled_encoder.shape)
 
         in_vals, out_resnet2 = 0, 0
         for i, layer in enumerate(self.resnet_2_layers):
@@ -137,22 +141,17 @@ class DeepLabV3P(nn.Module):
             else:
                 in_vals = out_resnet2
             out_resnet2 = layer(in_vals)
-        print("Post ResNet low-level: ", out_resnet2.shape)
 
         x7 = self.low_level_conv(out_resnet2)
         x7 = self.low_level_bn(x7)
         low_level = self.relu(x7)
-        print("Post low-level Conv: ", low_level.shape)
 
         decoder_concat = torch.cat([low_level, upsampled_encoder], dim=1)
-        print("Post low-level Concat: ", decoder_concat.shape)
 
         x8 = self.conv3_decoder(decoder_concat)
         x8 = self.bn_decoder(x8)
         x8 = self.relu(x8)
-        print("Post Decoder Conv: ", x8.shape)
 
         output = self.upsample_decoder(x8)
-        print("Post Decoder Upsample: ", output.shape)
 
         return output
